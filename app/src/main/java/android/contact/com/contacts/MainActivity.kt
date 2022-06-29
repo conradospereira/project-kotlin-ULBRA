f3d9bbaf44855e429dcbb9bb109c35da44d2ac54
conrado <conrado.spereira@gmail.com>
2022-06-29 13:33:20 -0300

    Preparing inputs on .kt.

--- app/src/main/java/android/contact/com/contacts/MainActivity.kt
+++ app/src/main/java/android/contact/com/contacts/MainActivity.kt
@@ -3,6 +3,7 @@ package android.contact.com.contacts
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 
@@ -11,7 +12,21 @@ class MainActivity : Activity() {
     var exibir_campo: TextView? = null
     var container_endereco: LinearLayout? = null
     var mudar_texto: LinearLayout? = null
-
+    var primeiro_nome: EditText? = null
+    var sobrenome: EditText? = null
+    var empresa: EditText? = null
+    var logradouro: EditText? = null
+    var numero: EditText? = null
+    var complemento: EditText? = null
+    var bairro: EditText? = null
+    var cidade: EditText? = null
+    var cep: EditText? = null
+    var uf: EditText? = null
+    var telefone_profissional: EditText? = null
+    var telefone_pessoal: EditText? = null
+    var email_profissional: EditText? = null
+    var email_pessoal: EditText? = null
+    var observacoes: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +34,6 @@ class MainActivity : Activity() {
 
         exibir_campo = findViewById(R.id.fechar_abrir)
         mudar_texto = findViewById(R.id.Show)
-
-
 
         exibir_campo?.setOnClickListener{
             if (container_endereco?.visibility == View.VISIBLE) {
