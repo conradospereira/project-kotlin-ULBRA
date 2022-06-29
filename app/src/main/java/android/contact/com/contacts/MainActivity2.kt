a7e8cc6bf429417cc74a817f42e0e143ea8ebe64
conrado <conrado.spereira@gmail.com>
2022-06-28 21:55:43 -0300

    Addin second screen.

---
+++ app/src/main/java/android/contact/com/contacts/MainActivity2.kt
@@ -0,0 +1,11 @@
+package android.contact.com.contacts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
